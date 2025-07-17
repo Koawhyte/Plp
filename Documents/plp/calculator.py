9b8e0f9d52b89adb7a404d8386a777cf0961a434
Koawhyte <nwanzecharity7@gmail.com>
2025-07-17 17:28:14 +0100

    my calculator

---
+++ Documents/plp/calculator.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+print("Let us calculate")
+
+num1 = float(input('enter your first number:'))
+num2 = float(input("enter your second number:"))
+
+sum_result = num1 + num2
+
+Difference_result = num1 - num2
+
+Product_result = num1 * num2
+
+Quotient_result = num1/num2
+
+print(sum_result)
+print(Difference_result)
+print(Product_result)
+print(Quotient_result)
+
+print("bye for now")
